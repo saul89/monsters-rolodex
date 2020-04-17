761690cdd8aa6f89f73f126b71c328afa99e78d3
SK26 <saul.lazzarini@gmail.com>
2020-04-17 21:34:42 +0200

    Updates

---
+++ precache-manifest.ee5394599dfb897470cd9b25b71e4ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c9b14b3837e49d73ca15f9c6fec293",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19afe0fc959b5aef71a7",
+    "url": "/monsters-rolodex/static/css/main.b0852760.chunk.css"
+  },
+  {
+    "revision": "838cdaa1bb67b13d9438",
+    "url": "/monsters-rolodex/static/js/2.0d6ef8e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0d6ef8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19afe0fc959b5aef71a7",
+    "url": "/monsters-rolodex/static/js/main.96a57ed5.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
